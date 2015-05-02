1487fff4ebadb166ec0360a55f7000eaed0d8bc9
Kihyun <inblue17@gmail.com>
2015-05-02 11:41:21 +0000

    타임라인 글 삭제 페에지 만들기

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -19,9 +19,9 @@ class WallController < ApplicationController
 	end
  	
 	def edit_complete
-		p = Post.find(params[:id]
+		p = Post.find(params[:id])
 		p.name = params[:writer_edit]
-		p.content = params[:content_edt]
+		p.content = params[:content_edit]
 		if p.save
 			redirect_to "/wall/posts"
 		else
@@ -30,6 +30,9 @@ class WallController < ApplicationController
 		end
 	end
 	
+	def delete
+		@post_delete = Post.find(params[:id])
+	end
 
   def posts
 		@posts = Post.all
